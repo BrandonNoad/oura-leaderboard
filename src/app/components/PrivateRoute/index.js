cadfebf04a5e1d2db3dbdb01c5a3bd49bd5c304b
Brandon Noad <Brandon.Noad@gmail.com>
2019-06-05 08:55:20 -0400

    Store access token

--- src/app/components/PrivateRoute/index.js
+++ src/app/components/PrivateRoute/index.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 import { navigate } from 'gatsby';
 import { selectSession } from '../../selectors';
 import ClientOAuth2 from 'client-oauth2';
+import _has from 'lodash/has';
+import Moment from 'moment';
 
 const PrivateRoute = ({ session, component: Component, ...rest }) => {
     const isLoggedIn = session !== null;
@@ -27,18 +29,43 @@ const PrivateRoute = ({ session, component: Component, ...rest }) => {
 
         (async function() {
             try {
+                // Get the access token from the fragment identifier.
                 // This will throw if there is no fragment identifier.
                 // May be undefined.
-                const { accessToken } = await ouraAuth.token.getToken(window.location.href);
+                const ouraAuthResult = await ouraAuth.token.getToken(window.location.href);
 
-                if (accessToken === undefined) {
+                if (ouraAuthResult.accessToken === undefined) {
                     throw new Error('Invalid access token');
                 }
 
-                setAccessToken(accessToken);
+                setAccessToken(ouraAuthResult.accessToken);
 
                 window.location.hash = '';
+
+                // Store the access token in localStorage.
+                // TODO: Associate access token with current user.
+                window.localStorage.setItem(
+                    'ouraLeaderboardOAuth2Result',
+                    JSON.stringify({
+                        accessToken: ouraAuthResult.accessToken,
+                        expires: ouraAuthResult.expires.getTime()
+                    })
+                );
             } catch (e) {
+                // Check localStorage for access token.
+                const oAuth2Result = JSON.parse(
+                    window.localStorage.getItem('ouraLeaderboardOAuth2Result')
+                );
+
+                // Is there an unexpired token?
+                if (
+                    _has(oAuth2Result, 'expires') &&
+                    Moment().isBefore(Moment(oAuth2Result.expires).subtract(1, 'days'))
+                ) {
+                    setAccessToken(oAuth2Result.accessToken);
+                    return;
+                }
+
                 // Redirect users to log in to Oura and authorize this app.
                 window.location.href = ouraAuth.token.getUri();
             }
